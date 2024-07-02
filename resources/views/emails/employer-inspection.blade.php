c8a7058e6e5f3b0a80aefd5e5bfaecbc45921b8c
Abdul5050 <tacticshustle@gmail.com>
2024-07-02 13:38:26 +0100

    email notification

--- resources/views/emails/employer-inspection.blade.php
+++ resources/views/emails/employer-inspection.blade.php
@@ -6,20 +6,21 @@
     <title>Client Inspection Notification Message</title>
 </head>
 <body>
-    <p>Dear Client,</p>
+    <p>Dear {{ $user->contact_firstname.' '.$user->contact_surname }},</p>
 
     <p>This is to notify you of a new inspection date from NIWA portal.</p>
 
     <p>Details of a new client document:</p>
     <ul>
-        <li>Inspection date: {{ $inspectionDatetime }}</li>
+        <li>Inspection date: {{ $service_app->date_of_inspection }}</li>
+        <li>Inspection Comments: {{ $service_app->comments_on_inspection }}</li>
     </ul>
 
     <p>Visit the url below to follow up and make payment form inspection</p>
 
-    <p><a href="https://eniwa.com.ng/payment/inspection">Inspection Status</a></p>
+    <p><a href="{{ env('DOCUMENT_URL') }}service-applications">Inspection Status</a></p>
 
     <p>Best regards,</p>
-    <p>NIWA EROM Portal</p>
+    <p>NIWA OPTIMA Portal</p>
 </body>
 </html>
